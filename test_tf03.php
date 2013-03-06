d7076b35307842a01b71e75695b6a7838b097940
TF03 <TF03@mail.ru>
2013-03-06 00:37:34 -0800

    New file TF03

---
+++ test_tf03.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: edegtyaryov
+ * Date: 06.03.13
+ * Time: 10:35
+ * To change this template use File | Settings | File Templates.
+ */
+echo "hello world!";
